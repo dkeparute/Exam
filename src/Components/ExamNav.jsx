72b588dd13f58f108b57ac5c4b9c9163081321c3
Dovilė <dovile.keparute@gmail.com>
2021-11-04 20:41:59 +0200

    filter

--- src/Components/ExamNav.jsx
+++ src/Components/ExamNav.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 function ExamNav({types, setFilter}) {
 
+const [filterValue, setFilterValue] = useState('');
+
+const selectFilter = e => {
+    setFilterValue(e.target.value);
+    setFilter(e.target.value);
+}
 
 
-    
     return (
         <div className='nav'>
             <span>Filter by type: </span>
-            <select>
+            <select onChange={selectFilter} value={filterValue}>
                 {types.map(t => <option key={t.type} value={t.type}>{t.type}</option>)}
                 <option value="">Select </option>
             </select>

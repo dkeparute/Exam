1cb0036f12b314c82f1b9b28211e65ae426dbbc0
Dovilė <dovile.keparute@gmail.com>
2021-11-09 13:25:37 +0200

    group by

--- src/Components/ExamStats.jsx
+++ src/Components/ExamStats.jsx
@@ -2,9 +2,9 @@ function ExamStats({ stats }) {
 
     return (
         <div className='stats'>
-            <span>Item count: {stats.count}</span>
-            <span>Item weight: {stats.weight} kg</span>
-            <span>Item average: {stats.average} kg</span>
+            <span>Item count: <i>{stats.count}</i></span>
+            <span>Item weight: <i>{stats.weight.toFixed(2)}</i> kg</span>
+            <span>Item average: <i>{stats.average.toPrecision(3)}</i> kg</span>
 
         </div>
     )
